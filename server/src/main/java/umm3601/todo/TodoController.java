64839819432adc16805a0b7fbd556e60c62dab7a
El Anderson <and10393@umn.edu>
2024-09-24 15:24:01 -0500

    Finishes first two tests for TodoControllerSpec - untested

--- server/src/main/java/umm3601/todo/TodoController.java
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -1,5 +1,24 @@
 package umm3601.todo;
 
+import com.mongodb.client.MongoDatabase;
+
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
 public class TodoController {
+
+  public TodoController(MongoDatabase db) {
+    //TODO Auto-generated constructor stub
+  }
+
+  public void addRoutes(Javalin mockServer) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'addRoutes'");
+  }
+
+public void getTodos(Context ctx) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getTodos'");
+}
 
 }
--- server/src/test/java/umm3601/todo/TodoControllerSpec.java
+++ server/src/test/java/umm3601/todo/TodoControllerSpec.java
@@ -1,28 +1,45 @@
 package umm3601.todo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
+import org.bson.Document;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import com.mongodb.MongoClientSettings;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
+import io.javalin.Javalin;
 import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
 import io.javalin.json.JavalinJackson;
+import umm3601.user.UserController;
 
 @SuppressWarnings({ "MagicNumber" })
 public class TodoControllerSpec {
-  private TodoController TodoController;
+  private TodoController todoController;
 
   private ObjectId samsId;
   private static MongoClient mongoClient;
@@ -59,4 +76,44 @@ public class TodoControllerSpec {
     mongoClient.close();
   }
 
+  @BeforeEach
+  void setupEach() throws IOException {
+    MockitoAnnotations.openMocks(this);
+    MongoCollection<Document> todoDocuments = db.getCollection("todos");
+    todoDocuments.drop();
+    List<Document> testTodos = new ArrayList<>();
+    testTodos.add(
+      new Document()
+          .append("owner", "Blanche")
+          .append("status", "complete")); //will need to check if this should be complete or true
+    samsId = new ObjectId();
+    Document sam = new Document()
+        .append("_id", samsId)
+        .append("owner", "Sam");
+
+    todoDocuments.insertMany(testTodos);
+    todoDocuments.insertOne(sam);
+
+    todoController = new TodoController(db);
+  }
+
+  @Test
+  public void canBuildController() throws IOException {
+    Javalin mockServer = Mockito.mock(Javalin.class);
+    todoController.addRoutes(mockServer);
+
+    verify(mockServer, Mockito.atLeast(2)).get(any(), any());
+  }
+
+  @Test
+  void canGetAllTodos() throws IOException {
+    when(ctx.queryParamMap()).thenReturn(Collections.emptyMap());
+
+    todoController.getTodos(ctx);
+
+    verify(ctx).json(todoArrayListCaptor.capture());
+    verify(ctx).status(HttpStatus.OK);
+
+    assertEquals(db.getCollection("todos").countDocuments(), todoArrayListCaptor.getValue().size());
+  }
 }
